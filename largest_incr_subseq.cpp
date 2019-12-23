6793608d94f3cb49d1810e7abd2c3005987fe7de
anmolkhemuka <anmolkhemuka8@gmail.com>
2019-12-23 17:53:57 +0530

    added function which will also print the longest subsequence

--- largest_incr_subseq.cpp
+++ largest_incr_subseq.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int LIS(int n, int arr[]);
+void printLIS(int n, int arr[]);
 int main()
 {
 
@@ -16,8 +18,8 @@ int main()
         {
             cin >> arr[i];
         }
-        cout << LIS(n, arr) << endl;
-        ;
+        
+        printLIS(n, arr);
     }
     return 0;
 }
@@ -27,7 +29,7 @@ int LIS(int n, int arr[])
     lis[0] = 1;
     for (int i = 1; i < n; i++) //filling the valuse in lis array in order(n^2) complexity.
     {
-        int max = 0;
+        int max = 1;
         for (int j = i - 1; j >= 0; j--)
         {
             if (arr[j] < arr[i])
@@ -45,4 +47,40 @@ int LIS(int n, int arr[])
     }
 
     return ans;
+}
+void printLIS(int n, int arr[])
+{
+    int lis[n];
+    // this array stores the length of the longest increasing subsequence ending at index i;
+    string liss[n];
+    lis[0] = 1;
+    liss[0] = to_string(arr[0]);
+    for (int i = 1; i < n; i++) //filling the valuse in lis array in order(n^2) complexity.
+    {
+        int max = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] < arr[i])
+            {
+                if (max < (lis[j] + 1))
+                    liss[i] = liss[j] + "," + to_string(arr[i]);
+                max = (max > (lis[j] + 1) ? max : (lis[j] + 1));
+            }
+            lis[i] = max;
+        }
+    }
+    int ans = lis[0];
+    string s = "";
+    for (int i = 1; i < n; i++) //finding the max length
+    {
+
+        if (lis[i] > ans)
+        {
+            ans = lis[i];
+            s = liss[i];
+        }
+    }
+
+    cout << ans << endl
+         << s << endl;
 }
